ad80530f8a4499ee24f8b8a90c7fd0dd24f266ac
Larissa Faria <lari.f4ria@gmail.com>
2025-05-07 19:02:48 -0300

    create test client should receive message from server

--- src/test/java/unit/model/ClientTest.java
+++ src/test/java/unit/model/ClientTest.java
@@ -5,7 +5,9 @@ import challenge.model.Server;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 
 public class ClientTest {
@@ -51,6 +53,39 @@ public class ClientTest {
         serverThread.start();
         client.setSocket();
         Assertions.assertTrue(client.send(message));
+        server.close();
+    }
+
+    @Test
+    public void clientShouldReceiveMessageFromServer() throws IOException, InterruptedException {
+        int port = 3333;
+        String ip = "localhost";
+        Client client = new Client(ip, port);
+        String message = "Mensagem do servidor"+"\n\r";
+        Server server = new Server(new ServerSocket(port));
+        Thread thread = serverThread(server);
+        thread.start();
+        Thread.sleep(100);
+        client.setSocket();
+        thread.join();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(client.getSocket().getInputStream()));
+
+        Assertions.assertTrue(client.isConnected());
+        Thread readerThread = new Thread(()->{
+            try {
+                String received = reader.readLine();
+                Assertions.assertEquals(message, received);
+                reader.close();
+            } catch (IOException e) {
+                e.getMessage();
+            }
+        });
+        readerThread.start();
+
+        server.send(message, client.getSocket());
+
+        readerThread.join(10);
+        client.close();
         server.close();
     }
 }
